d69d60fee42747f1d3cb3dca6328d426fd55e2ee
diego-eco <59991204+diego-eco@users.noreply.github.com>
2020-08-01 10:58:15 -0500

    update

--- app.R
+++ app.R
@@ -32,7 +32,7 @@ ensanut_ap <- read.csv(file = "data/ensanut_areas_peq.csv",
 #    dplyr::rename(CVEGEO = mun,Obesidad=obesidad,Hipertensión=hipertension,Diabetes=diabetes)
 
 #Unir ambas bases
-datos<-base::merge(x=mun_nac,y=ensanut_ap,by=c("CVEGEO"))
+datos<-base::merge(x=mun_nac,y=ensanut_ap,by="CVEGEO")
 
 #### End Global R frame 
 
